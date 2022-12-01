6d44f67a7521a0010b0b96158682a9afaf61fa4b
suqun zhang <susizhang2017@gmail.com>
2022-12-01 15:17:32 +0100

    adding detail files

--- src/components/SuperDetail.js
+++ src/components/SuperDetail.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import _ from "lodash";
 
 function SuperDetail() {
   const { id, info } = useParams();
   const [infoDetail, setInfoDetail] = useState();
+
   useEffect(() => {
     axios
       .get(`http://localhost:5020/pokemon/${id}/${info}`)
@@ -16,13 +18,31 @@ function SuperDetail() {
 
   if (!infoDetail) return <h2>Loading...</h2>;
 
-  //   console.log(" ", infoDetail);
+  const detailInfo = () => {
+    if (info === "name") {
+      return <h1>{infoDetail}</h1>;
+    } else if (info === "type") {
+      return infoDetail.map((item, index) => (
+        <div key={index}>
+          <p>{item}</p>
+        </div>
+      ));
+    } else if (info === "base") {
+      const baseArr = _.toPairs(infoDetail);
+      // console.log(" ", baseArr);
+      return (
+        <div>
+          {baseArr.map((item, index) => (
+            <p key={index}>{item.join(" : ")}</p>
+          ))}
+        </div>
+      );
+    }
+  };
 
-  return (
-    <>
-      <div>{infoDetail}</div>
-    </>
-  );
+  // console.log(" ", infoDetail);
+
+  return <>{detailInfo()}</>;
 }
 
 export default SuperDetail;
